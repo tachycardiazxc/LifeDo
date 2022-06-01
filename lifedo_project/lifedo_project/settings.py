d08f1ccc9b64a9eb04b146ce9657cb2b4af199a2
devourmealive <foxmanlisichka@yandex.ru>
2022-06-01 21:07:23 +0300

    LifeDo -> Changed requirements.txt and settings.py(now DB engine is postgresql)

--- lifedo_project/lifedo_project/settings.py
+++ lifedo_project/lifedo_project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'lifedo_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lifedo_db',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
