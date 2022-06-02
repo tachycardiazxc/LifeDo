bc4118f0a5e0646a8055ddd55295fe429a4f8a2a
devourmealive <foxmanlisichka@yandex.ru>
2022-06-03 01:47:58 +0300

    LifeDo -> Created index page frontend and url routing there

--- lifedo_project/index_app/views.py
+++ lifedo_project/index_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django import views
 
-# Create your views here.
+
+class IndexView(views.View):
+
+    def get(self, request):
+        return render(request, template_name='index.html', context={
+
+        })
